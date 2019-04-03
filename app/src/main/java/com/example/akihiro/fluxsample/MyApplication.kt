49db688546d514c2502633bea30a9456d990f935
akihiro.ngit config --global user.name akihiro.ngit config --global user.name akihiro.n <akihiro.n@arsaga.jp>
2019-04-03 14:45:20 +0900

    DI用のModuleディレクトリを追加

--- app/src/main/java/com/example/akihiro/fluxsample/MyApplication.kt
+++ app/src/main/java/com/example/akihiro/fluxsample/MyApplication.kt
@@ -1,27 +1,10 @@
 package com.example.akihiro.fluxsample
 
 import android.app.Application
-import com.example.akihiro.fluxsample.application.actioncreator.ItemActionCreator
-import flux.Dispatcher
-import com.example.akihiro.fluxsample.domain.repository.ItemRepository
-import com.example.akihiro.fluxsample.domain.repository.ItemRepositoryImpl
-import com.example.akihiro.fluxsample.domain.store.ItemStore
-import com.example.akihiro.fluxsample.domain.usecase.ItemUseCase
-import com.example.akihiro.fluxsample.domain.usecase.ItemUseCaseImpl
-import com.example.akihiro.fluxsample.infra.API
-import com.example.akihiro.fluxsample.ui.MainViewModel
-import flux.Action
-import io.reactivex.subjects.PublishSubject
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
+import com.example.akihiro.fluxsample.module.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.module.Module
-import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MyApplication : Application() {
 
@@ -51,58 +34,5 @@ class MyApplication : Application() {
         }
     }
 
-    private val clientModule: Module by lazy {
-        module {
-            single<API> {
-                val httpLoggingInterceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
-                val okHttpClient = OkHttpClient.Builder()
-                    .addInterceptor(httpLoggingInterceptor)
-                    .build()
 
-                Retrofit.Builder()
-                    .baseUrl(getString(R.string.base_url))
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .client(okHttpClient)
-                    .build()
-                    .create(API::class.java)
-            }
-        }
-    }
-
-    private val repositoryModule: Module by lazy {
-        module {
-            single<ItemRepository> { ItemRepositoryImpl(get()) }
-        }
-    }
-
-    private val useCaseModule: Module by lazy {
-        module {
-            single<ItemUseCase> { ItemUseCaseImpl(get()) }
-        }
-    }
-
-    private val actionCreatorModule: Module by lazy {
-        module {
-            single { ItemActionCreator(get()) }
-        }
-    }
-
-    private val dispatcherModule: Module by lazy {
-        module {
-            single<Dispatcher> { PublishSubject.create<Action>() }
-        }
-    }
-
-    private val storeModule: Module by lazy {
-        module {
-            single { ItemStore() }
-        }
-    }
-
-    private val viewModelModule: Module by lazy {
-        module {
-            single { MainViewModel(get(), get()) }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/akihiro/fluxsample/module/Module.kt
@@ -0,0 +1,77 @@
+package com.example.akihiro.fluxsample.module
+
+import com.example.akihiro.fluxsample.MyApplication
+import com.example.akihiro.fluxsample.R
+import com.example.akihiro.fluxsample.application.actioncreator.ItemActionCreator
+import com.example.akihiro.fluxsample.domain.repository.ItemRepository
+import com.example.akihiro.fluxsample.domain.repository.ItemRepositoryImpl
+import com.example.akihiro.fluxsample.domain.store.ItemStore
+import com.example.akihiro.fluxsample.domain.usecase.ItemUseCase
+import com.example.akihiro.fluxsample.domain.usecase.ItemUseCaseImpl
+import com.example.akihiro.fluxsample.infra.API
+import com.example.akihiro.fluxsample.ui.MainViewModel
+import flux.Action
+import flux.Dispatcher
+import io.reactivex.subjects.PublishSubject
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+val clientModule: Module by lazy {
+    module {
+        single<API> {
+            val httpLoggingInterceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(MyApplication.getApplication.getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(okHttpClient)
+                .build()
+                .create(API::class.java)
+        }
+    }
+}
+
+val repositoryModule: Module by lazy {
+    module {
+        single<ItemRepository> { ItemRepositoryImpl(get()) }
+    }
+}
+
+val useCaseModule: Module by lazy {
+    module {
+        single<ItemUseCase> { ItemUseCaseImpl(get()) }
+    }
+}
+
+val actionCreatorModule: Module by lazy {
+    module {
+        single { ItemActionCreator(get()) }
+    }
+}
+
+val dispatcherModule: Module by lazy {
+    module {
+        single<Dispatcher> { PublishSubject.create<Action>() }
+    }
+}
+
+val storeModule: Module by lazy {
+    module {
+        single { ItemStore() }
+    }
+}
+
+ val viewModelModule: Module by lazy {
+    module {
+        single { MainViewModel(get(), get()) }
+    }
+}
