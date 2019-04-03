57a0a940cea1137ca18389fa5ff62816bc08cc64
akihiro.ngit config --global user.name akihiro.ngit config --global user.name akihiro.n <akihiro.n@arsaga.jp>
2019-04-03 14:38:26 +0900

    コメント追加

--- app/src/main/java/com/example/akihiro/fluxsample/utility/ImageViewExtension.kt
+++ app/src/main/java/com/example/akihiro/fluxsample/utility/ImageViewExtension.kt
@@ -4,13 +4,15 @@ import android.databinding.BindingAdapter
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.widget.ImageView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.net.HttpURLConnection
 import java.net.URL
 
+/**
+ * ImageViewクラスのExtension
+ * URL取得した画像を、ImageViewに表示させる
+ * @param url
+ */
 @BindingAdapter("app:imageUrl")
 fun ImageView.imageUrl(url: String?) {
     url?: return
@@ -19,7 +21,12 @@ fun ImageView.imageUrl(url: String?) {
     }
 }
 
-private fun fetchBitmapAsync(url: String) = GlobalScope.async(Dispatchers.Default) {
+/**
+ * Http通信を非同期で行い、画像をBitmap形式で取得する
+ * @param url
+ * @return
+ */
+private fun fetchBitmapAsync(url: String): Deferred<Bitmap?> = GlobalScope.async(Dispatchers.Default) {
     var bitmap: Bitmap? = null
     (URL(url).openConnection() as HttpURLConnection)
         .also { it.setRequestConfiguration() }
@@ -36,6 +43,9 @@ private fun fetchBitmapAsync(url: String) = GlobalScope.async(Dispatchers.Defaul
     return@async bitmap
 }
 
+/**
+ * Http通信に関する設定を定義
+ */
 private fun HttpURLConnection.setRequestConfiguration() {
     also { httpURLConnection ->
         httpURLConnection.requestMethod = "GET"
